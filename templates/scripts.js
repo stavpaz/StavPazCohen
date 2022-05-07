b167e18edc109e59fa0823aede723ed00a2a3ba6
סתיו כהן <stavpaz@post.bgu.ac.il>
2022-05-07 22:32:16 +0300

    fix images slider

---
+++ templates/scripts.js
@@ -0,0 +1,8 @@
+const activepage=window.location.pathname;
+const navlink=document.querySelectorAll('nav a').forEach(link=>{
+    if(link.herf.includes('${activepage}')){
+        link.addEventListener(function(){
+            headerhome.s
+        })
+    }
+})
